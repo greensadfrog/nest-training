5ead491b72dfce7b3c69257915ca7e40ef5bbcdf
Mikhail Dudka <deanwenchester13@gmail.com>
2023-02-05 23:50:33 +0200

    implemented JwtAuthGuard in order to restrict access to the necessary endpoints; access to the /users endpoint is restricted to unauthorized persons

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import {forwardRef, Module} from '@nestjs/common';
+import {AuthService} from './auth.service';
+import {AuthController} from './auth.controller';
 import {UsersModule} from "../users/users.module";
 import {JwtModule} from "@nestjs/jwt";
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController],
-  imports: [
-      UsersModule,
-      JwtModule.register({
-          secret: process.env.JWT_PRIVATE_KEY || 'SECRET',
-          signOptions: {
-              expiresIn: '24h'
-          }
-      })
-  ]
+    providers: [AuthService],
+    controllers: [AuthController],
+    imports: [
+        forwardRef(() => UsersModule),
+        JwtModule.register({
+            secret: process.env.JWT_PRIVATE_KEY || 'SECRET',
+            signOptions: {
+                expiresIn: '24h'
+            }
+        })
+    ],
+    exports: [
+        AuthService,
+        JwtModule
+    ]
 })
-export class AuthModule {}
+export class AuthModule {
+}
