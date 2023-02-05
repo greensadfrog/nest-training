06408504c4dae49a2f9a5e24fbd83dec19e6cd0c
Mikhail Dudka <deanwenchester13@gmail.com>
2023-02-05 23:19:44 +0200

    created auth module, implemented authorization with jwt token

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import {User} from "./users/user.model";
 import { RolesModule } from './roles/roles.module';
 import {Role} from "./roles/roles.model";
 import {UserRoles} from "./roles/user-roles.model";
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -27,7 +28,8 @@ import {UserRoles} from "./roles/user-roles.model";
             autoLoadModels: true
         }),
         UsersModule,
-        RolesModule
+        RolesModule,
+        AuthModule
     ],
 })
 export class AppModule {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import {Module} from '@nestjs/common';
+import {UsersController} from './users.controller';
+import {UsersService} from './users.service';
 import {SequelizeModule} from "@nestjs/sequelize";
 import {User} from "./user.model";
 import {Role} from "../roles/roles.model";
@@ -8,11 +8,14 @@ import {UserRoles} from "../roles/user-roles.model";
 import {RolesModule} from "../roles/roles.module";
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
-  imports: [
-      SequelizeModule.forFeature([User, Role, UserRoles]),
-      RolesModule
-  ]
+    controllers: [UsersController],
+    providers: [UsersService],
+    imports: [
+        SequelizeModule.forFeature([User, Role, UserRoles]),
+        RolesModule
+    ],
+    exports: [
+        UsersService
+    ]
 })
 export class UsersModule {}
