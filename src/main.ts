7f9fb91fe684effd4136b97684f1474a909b0de4
Mikhail Dudka <deanwenchester13@gmail.com>
2023-02-05 18:35:51 +0200

    initially configured swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import {NestFactory} from "@nestjs/core";
 
 import {AppModule} from "./app.module";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 
 async function  start() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Nestjs Tutorial')
+        .setDescription('REST API Documentation')
+        .setVersion('1.0.0')
+        .addTag('ULBI TV')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('/api/docs', app, document)
 
     await app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`))
 }
