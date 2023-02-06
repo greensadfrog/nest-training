2d5b559ca7cb6f661dcf9d7cf4123df613e8d0d8
Mikhail Dudka <deanwenchester13@gmail.com>
2023-02-06 02:23:04 +0200

    implemented file handling and serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import {Module} from "@nestjs/common";
 import {SequelizeModule} from "@nestjs/sequelize";
 import {ConfigModule} from "@nestjs/config";
 
-import { UsersModule } from './users/users.module';
+import {UsersModule} from './users/users.module';
 import {User} from "./users/user.model";
-import { RolesModule } from './roles/roles.module';
+import {RolesModule} from './roles/roles.module';
 import {Role} from "./roles/roles.model";
 import {UserRoles} from "./roles/user-roles.model";
-import { AuthModule } from './auth/auth.module';
+import {AuthModule} from './auth/auth.module';
+import {PostsService} from './posts/posts.service';
+import {PostsModule} from './posts/posts.module';
+import {Post} from "./posts/posts.model";
+import {FilesModule} from './files/files.module';
+import {ServeStaticModule} from "@nestjs/serve-static";
+import * as path from 'path';
 
 
 @Module({
@@ -17,6 +23,9 @@ import { AuthModule } from './auth/auth.module';
         ConfigModule.forRoot({
             envFilePath: `.${process.env.NODE_ENV}.env`
         }),
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname, 'static')
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
             host: process.env.POSTGRES_HOST,
@@ -24,14 +33,15 @@ import { AuthModule } from './auth/auth.module';
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [User, Role, UserRoles],
+            models: [User, Role, UserRoles, Post],
             autoLoadModels: true
         }),
         UsersModule,
         RolesModule,
-        AuthModule
+        AuthModule,
+        PostsModule,
+        FilesModule
     ],
 })
 export class AppModule {
-
 }
